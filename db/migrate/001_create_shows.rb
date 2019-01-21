543980d1467e15f2364caa0d8050998cae3efed1
Stef Claus <stef.claus@gmail.com>
2019-01-21 21:28:01 +0000

    Done.

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,10 @@
+class Shows < ActiveRecord::Migration[5.1]
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :netword
+      t.string :day
+      t.integer :rating
+    end #ends iteration
+  end #ends method
+end #ends class
