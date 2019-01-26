12ba1e7463ae730107ed2575271e27b951fab115
Stef Claus <stef.claus@gmail.com>
2019-01-26 17:17:12 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -25,7 +25,7 @@ class Show < ActiveRecord::Base
 
   def self.ratings_sum
     #ratings_sum: returns the sum of all of the ratings.
-    Show.sum(ratings)
+    Show.sum(rating:)
   end
 
   def self.popular_shows
